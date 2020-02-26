8adcfb3a21b683942ffca827ca14d4ab426ab85e
Ryan Hsueh <ryan.hsueh@gogoro.com>
2020-02-26 11:46:58 +0800

    modified SlideViewActivity

--- app/src/main/java/com/example/ryanhsueh/androidviewplayground/SlideViewActivity.java
+++ app/src/main/java/com/example/ryanhsueh/androidviewplayground/SlideViewActivity.java
@@ -4,10 +4,15 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.ryanhsueh.androidviewplayground.customView.SlideView;
 
 public class SlideViewActivity extends AppCompatActivity {
+
+    boolean isUp = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +40,43 @@ public class SlideViewActivity extends AppCompatActivity {
 
         //使用Scroll来进行平滑移动
 //        customView.smoothScrollTo(-400,0);
+
+
+
+        ImageView clickView = findViewById(R.id.clickview);
+        clickView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isUp) down(v); else up(v);
+                Toast.makeText(SlideViewActivity.this, "Click it !!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void up(View view) {
+        isUp = true;
+
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(view, "translationX", 0.0f, 300.0f);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(view, "translationY", 0.0f, -500.0f);
+        ObjectAnimator animator3 = ObjectAnimator.ofFloat(view, "scaleX", 0.5f, 2.0f);
+        ObjectAnimator animator4 = ObjectAnimator.ofFloat(view, "scaleY", 0.5f, 2.0f);
+        ObjectAnimator animator5 = ObjectAnimator.ofFloat(view, "rotationY", 0.0f, 90.0f, 0f);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(300);
+        animatorSet.play(animator1).with(animator2).with(animator3).with(animator4).before(animator5);
+        animatorSet.start();
+    }
+
+    private void down(View view) {
+        isUp = false;
+
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(view, "translationX", 300.0f, 0.0f);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(view, "translationY", -500.0f, 0.0f);
+        ObjectAnimator animator3 = ObjectAnimator.ofFloat(view, "scaleX", 2.5f, 1.0f);
+        ObjectAnimator animator4 = ObjectAnimator.ofFloat(view, "scaleY", 2.5f, 1.0f);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(300);
+        animatorSet.play(animator1).with(animator2).with(animator3).with(animator4);
+        animatorSet.start();
     }
 }
